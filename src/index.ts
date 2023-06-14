2e664cfb05c67b8c5a4595c33e9f659c9cba250b
Nicholas <nick.saravia@gmail.com>
2023-06-13 22:14:58 -0400

    meant to have this stuff on server.ts

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,8 @@
 import app from './server';
-import express from 'express';
 import * as dotenv from 'dotenv';
-import cors from 'cors';
-import morgan from 'morgan';
 
 // load env contents into process
 dotenv.config();
-
-// cors
-app.use(cors());
-
-// logger
-app.use(morgan('dev'));
-
-/**
- * middleware to allow express to interpret
- * json when in request body
- */
-app.use(express.json());
-
-/**
- * this option below is not stirctly needed
- * but a good practice when building api's for
- * our "old-school" buddies
- */
-app.use(express.urlencoded({ extended: true }));
 
 app.listen(3000, () => {
     console.log('server listening on port 3000');
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,30 @@
-import express from "express";
+import express from 'express';
+import cors from 'cors';
+import morgan from 'morgan';
 
 const app = express();
 
-app.get("/", (req, res) => {
-  return res.json({ message: "hello buddy" });
+// cors
+app.use(cors());
+
+// logger
+app.use(morgan('dev'));
+
+/**
+ * middleware to allow express to interpret
+ * json when in request body
+ */
+app.use(express.json());
+
+/**
+ * this option below is not stirctly needed
+ * but a good practice when building api's for
+ * our "old-school" buddies
+ */
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    return res.json({ message: 'hello buddy' });
 });
 
 export default app;
