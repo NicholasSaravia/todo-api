3347cc6644e8996fd00ad9a4c8a5bc33dcf4452c
Nicholas Saravia <nickskies@Laurens-Air.lan>
2023-06-13 22:02:33 -0400

    json interpreter & urlencoded

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,26 @@
-import app from "./server";
-import * as dotenv from "dotenv";
+import app from './server';
+import express from 'express';
+import * as dotenv from 'dotenv';
 
+// load env contents into process
 dotenv.config();
 
+// cors
+// logger
+
+/**
+ * middleware to allow express to interpret
+ * json when in request body
+ */
+app.use(express.json());
+
+/**
+ * this option below is not stirctly needed
+ * but a good practice when building api's for
+ * our "old-school" buddies
+ */
+app.use(express.urlencoded({ extended: true }));
+
 app.listen(3000, () => {
-  console.log("server listening on port 3000");
+    console.log('server listening on port 3000');
 });
