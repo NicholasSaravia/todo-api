0aff7b013a67f165988c441b7266f4b987d3645c
Nicholas <nick.saravia@gmail.com>
2023-06-15 09:23:02 -0400

    small bit of input validation on todo

--- src/router/todo/router.ts
+++ src/router/todo/router.ts
@@ -1,10 +1,15 @@
 import { Router } from 'express';
 import { createTodo, deleteTodo, getOneTodo, getTodos, updateTodo } from '.';
+import { body } from 'express-validator';
 
 export const todoRouter = Router();
 
 todoRouter.get('/todo', getTodos);
 todoRouter.get('/todo/:id', getOneTodo);
-todoRouter.post('/todo', createTodo);
-todoRouter.put('/todo/:id', updateTodo);
+todoRouter.post('/todo', [body('value').isString().trim()], createTodo);
+todoRouter.put(
+    '/todo/:id',
+    [body('value').optional().isString().trim()],
+    updateTodo
+);
 todoRouter.delete('/todo/:id', deleteTodo);
