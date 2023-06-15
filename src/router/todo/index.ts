8cf04e168c985dcb19f4e2a766af6be61b2401b0
Nicholas <nick.saravia@gmail.com>
2023-06-15 09:12:13 -0400

    todo create/update/delete & routes

--- src/router/todo/index.ts
+++ src/router/todo/index.ts
@@ -1,32 +1,108 @@
-import { Request, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
 import { prisma } from '../../db';
 
-export const getTodos = async (req: Request, res: Response) => {
-    const todos = await prisma.todo.findMany({
-        where: {
-            userId: req.user.id
-        }
-    });
-    res.status(200).json({ data: todos });
-};
-
-export const getOneTodo = async (req: Request, res: Response) => {
-    const todo = await prisma.todo.findUnique({
-        where: {
-            id_userId: {
-                id: req.params.id,
+export const getTodos = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => {
+    try {
+        const todos = await prisma.todo.findMany({
+            where: {
                 userId: req.user.id
             }
-        }
-    });
-
-    if (!todo) {
-        res.status(400).json({
-            message:
-                'are you sure you are searching for something that is yours'
         });
-        return;
+        res.status(200).json({ data: todos });
+    } catch (error) {
+        next(error);
     }
+};
 
-    return res.status(200).json({ data: todo });
+export const getOneTodo = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => {
+    try {
+        const todo = await prisma.todo.findUnique({
+            where: {
+                id_userId: {
+                    id: req.params.id,
+                    userId: req.user.id
+                }
+            }
+        });
+
+        if (!todo) {
+            res.status(400).json({
+                message:
+                    'are you sure you are searching for something that is yours'
+            });
+            return;
+        }
+
+        res.status(200).json({ data: todo });
+    } catch (error) {
+        next(error);
+    }
+};
+
+export const createTodo = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => {
+    try {
+        const todo = await prisma.todo.create({
+            data: {
+                ...req.body,
+                userId: req.user.id
+            }
+        });
+
+        res.status(201).json({ data: todo });
+    } catch (error) {
+        next(error);
+    }
+};
+
+export const updateTodo = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => {
+    try {
+        const todo = await prisma.todo.update({
+            data: req.body,
+            where: {
+                id_userId: {
+                    id: req.params.id,
+                    userId: req.user.id
+                }
+            }
+        });
+        res.status(200).json({ data: todo });
+    } catch (error) {
+        next(error);
+    }
+};
+
+export const deleteTodo = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => {
+    try {
+        const todo = await prisma.todo.delete({
+            where: {
+                id_userId: {
+                    id: req.params.id,
+                    userId: req.user.id
+                }
+            }
+        });
+        res.status(200).json({ data: todo });
+    } catch (error) {
+        next(error);
+    }
 };
--- src/router/todo/router.ts
+++ src/router/todo/router.ts
@@ -1,7 +1,10 @@
 import { Router } from 'express';
-import { getOneTodo, getTodos } from '.';
+import { createTodo, deleteTodo, getOneTodo, getTodos, updateTodo } from '.';
 
 export const todoRouter = Router();
 
 todoRouter.get('/todo', getTodos);
 todoRouter.get('/todo/:id', getOneTodo);
+todoRouter.post('/todo', createTodo);
+todoRouter.put('/todo/:id', updateTodo);
+todoRouter.delete('/todo/:id', deleteTodo);
