5fb13dd928985c318f987c0835a2cdebb5308441
Nicholas <nick.saravia@gmail.com>
2023-07-05 16:39:13 -0400

    project api route colors and router hook up

--- src/router.ts
+++ src/router.ts
@@ -1,7 +1,9 @@
 import { Router } from 'express';
 import { todoRouter } from './router/todo/router';
+import { projectRouter } from './router/project/router';
 const router = Router();
 
 router.use(todoRouter);
+router.use(projectRouter);
 
 export default router;
